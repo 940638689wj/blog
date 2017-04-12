2624aff72bf781e113b20b9c3e4342a30b06350f
940638689wj <940638689@qq.com>
2017-04-12 14:53:01 +0800

    first commit

---
+++ vue/src/components/inside/List.vue
@@ -0,0 +1,75 @@
+<template>
+<div>
+  <NavDiv></NavDiv>
+  <br><br><br><br>
+  <el-row type="flex" class="blogList" justify="center" v-for="blog in blogList">
+    <!-- <el-col :span="6"><div class="grid-content bg-purple"></div></el-col> -->
+    <el-col :span="12"><div class="grid-content bg-purple-light">
+      <p class="title"><b>{{blog.title}}</b></p>
+      <p class="author">
+        <i class="el-icon-edit"></i>{{blog.author}}&nbsp;&nbsp;&nbsp;&nbsp;
+        <i class="el-icon-time"></i>{{blog.createTime}}
+      </p>
+      <p>{{blog.content}}</p>
+      <p class="author"><i class="el-icon-document"></i>123</p>
+      <hr style="height:1px;border:none;border-top:1px solid #dddddd;" />
+    </div></el-col>
+    <!-- <el-col :span="6"><div class="grid-content bg-purple"></div></el-col> -->
+  </el-row>
+</div>
+</template>
+
+<script>
+import NavDiv from './components/Nav'
+
+export default {
+  name: 'list',
+  data () {
+    return {
+      activeIndex: '1',
+      searchContent: '',
+      blogList: [
+        {
+          title: '这是标题这是标题这是标题这是标题这是标题~~~',
+          content: '这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容...',
+          author: '作者',
+          createTime: '2017-04-02'
+        },
+        {
+          title: '这是标题这是标题这是标题这是标题这是标题~~~',
+          content: '这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容这是预览内容...',
+          author: '作者',
+          createTime: '2017-04-02'
+        }
+      ]
+    }
+  },
+  components: {
+    NavDiv
+  },
+  methods: {
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleIconClick () {
+      console.log('点击搜索')
+    }
+  },
+  created () {
+  }
+}
+</script>
+
+<style scoped>
+.blogList{
+
+}
+.title{
+  font-size: 25px;
+}
+.author{
+  color: #cccccc;
+  font-size: 14px;
+}
+
+</style>
---
+++ vue/src/components/inside/components/Nav.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <el-menu :default-active="activeIndex" class="el-menu" mode="horizontal" @select="handleSelect">
+      <el-menu-item index="1">首页</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">文章分类</template>
+        <el-menu-item index="2-1">分类1</el-menu-item>
+        <el-menu-item index="2-2">分类2</el-menu-item>
+        <el-menu-item index="2-3">分类3</el-menu-item>
+      </el-submenu>
+    </el-menu>
+
+    <!-- logo或标题 -->
+    <div class="logo">
+      <b>尤琳杰のblog</b>
+    </div>
+
+    <!-- 搜索栏 -->
+    <div class="search">
+      <el-input
+      placeholder="搜索"
+      icon="search"
+      v-model="searchContent"
+      :on-icon-click="handleIconClick">
+      </el-input>
+    </div>
+
+    <!-- 登录注册按钮 -->
+    <div class="buttonGroup">
+      <el-button type="default">登录</el-button>
+      <el-button type="default">注册</el-button>
+    </div>
+
+    <div class="write">
+      <el-button type="primary" icon="edit">写文章</el-button>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'nav',
+  data () {
+    return {
+      activeIndex: '1',
+      searchContent: ''
+    }
+  },
+  methods: {
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleIconClick () {
+      console.log('点击搜索')
+    }
+  },
+  created () {
+  }
+}
+</script>
+
+<style scoped>
+.logo{
+  position: fixed;
+  left: 30px;
+  top: 15px; 
+  font-size: 20px;
+  font-style: italic;
+  color: #EA6F5A;
+}
+.search{
+  position: fixed;
+  right: 340px;
+  top: 12px; 
+}
+.buttonGroup{
+  position: fixed;
+  right: 30px;
+  top: 12px;
+}
+.write{
+  position: fixed;
+  right: 230px;
+  top: 12px;
+}
+.el-menu{
+  padding-left: 300px;
+}
+</style>
